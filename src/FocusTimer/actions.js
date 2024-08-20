cc83d7390960b7594b273ae9be3ed873ab0b3d64
Vitor <77864032+Vitorcastanhos@users.noreply.github.com>
2024-08-20 14:53:23 -0300

    starting countdown

--- src/FocusTimer/actions.js
+++ src/FocusTimer/actions.js
@@ -1,19 +1,19 @@
-import state from "./state";
+import state from "./state.js";
+import * as timer from "./timer.js";
 
 export function toggleRunning() {
 	state.isRunning = document.documentElement.classList.toggle("running");
-	console.log(state.isRunning);
+
+	timer.countdown();
 }
 
 export function reset() {
-    state.isRunning = false;
-    document.documentElement.classList.remove("running");
+	state.isRunning = false;
+	document.documentElement.classList.remove("running");
 }
 
-export function set() {
-	
-}
+export function set() {}
 
 export function toggleMusic() {
-    state.isMute = document.documentElement.classList.toggle("music-on");
+	state.isMute = document.documentElement.classList.toggle("music-on");
 }
